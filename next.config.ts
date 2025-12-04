cbd27262002f587afcc4bdf975ad1146e03cd3d2
faraponov-agima <a.faraponov@agima.ru>
2025-12-04 11:13:01 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/info/glioma",
   turbopack: {
     rules: {
       "*.svg": {
